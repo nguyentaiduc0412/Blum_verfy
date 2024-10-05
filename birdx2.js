bf0b23232547d95c2fdc9a6072b5c7e6223241ad
nguyentaiduc0412 <48790583+nguyentaiduc0412@users.noreply.github.com>
2024-10-06 03:39:05 +0700

    Create birdx2.js

---
+++ birdx2.js
@@ -0,0 +1,20 @@
+const VERIFY_TASK_MAPPING = {
+  "66fd20ff1480eebd1abede1e": { slug: "play-1win", point: 500, channelId: "" },
+  "66eec3277d5aa75a64e36f95": { slug: "none", point: 200, channelId: "" },
+  "66eec33f7d5aa75a64e39d86": { slug: "none", point: 200, channelId: "" },
+  "66fd1c56b2b16312146d8c22": { slug: "none", point: 200, channelId: "" },
+  "66fd1c701480eebd1abe2486": { slug: "none", point: 200, channelId: "" },
+  "66b04e036550ead93430a8cc": { slug: "none", point: 400, channelId: "" },
+  "66d213aaf9ec66b0f25e987c": { slug: "none", point: 300, channelId: "" },
+  "66fc8a30b2b16312145e88ae": { slug: "none", point: 200, channelId: "" },
+  "66fc2197850f0f8b3db3632d": { slug: "none", point: 300, channelId: "" },
+  "66fa5bca0e69d08283713b3f": { slug: "none", point: 200, channelId: "" },
+  "66b4c3cadba6f5ab3cdaada0": { slug: "invite-friend-1", point: 1000, channelId: "" },
+  "66b4c448dba6f5ab3cdab3a5": { slug: "invite-friend-5", point: 5000, channelId: "" },
+  "66b4c4950e891a4ee017d280": { slug: "invite-friend-10", point: 10000, channelId: "" },
+  "66f68bd2132e8749f56f890b": { slug: "play-gumart", point: 500, channelId: "" },
+  "66fa588ac0390c7153541021": { slug: "none", point: 200, channelId: "" },
+  "66e9be65f6a09bc16176364a": { slug: "none", point: 100, channelId: "" }
+};
+
+module.exports = { VERIFY_TASK_MAPPING };
