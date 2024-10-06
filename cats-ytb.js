f90c215abcd6ebef84d8d6cfe45184f0ef89c5dc
nguyentaiduc0412 <48790583+nguyentaiduc0412@users.noreply.github.com>
2024-10-07 01:01:44 +0700

    Update cats-ytb.js

--- cats-ytb.js
+++ cats-ytb.js
@@ -3,7 +3,11 @@ const VERIFY_TASK_MAPPING = {
  146: "dip",
  153: "ABSTRACT",
  148: "AIRNODE",
- 149: "WEI"
+ 149: "WEI",
+ 158: "AFFILIATE",
+ 154: "AUCTION",
+ 155:"AUDIT",
+ 159:"BAG"
 };
 
 module.exports = { VERIFY_TASK_MAPPING };
