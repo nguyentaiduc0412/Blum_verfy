8436820830c4d2f04fef4bcb2c55451e8ab734fd
BNB <49827182+bnbbinh1993@users.noreply.github.com>
2024-10-27 22:30:20 +0700

    Update notpixel.js

--- notpixel.js
+++ notpixel.js
@@ -2,6 +2,7 @@ const config_notpixel = {
   "xTopLeft": 107,
   "yTopLeft": 282,
   "xBottomRight": 234,
-  "yBottomRight": 308
+  "yBottomRight": 308,
+  "templateId":1863187931
 }
 module.exports = { config_notpixel };
