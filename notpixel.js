47ed029255883d4d69fe59d608ea44debb038dfa
BNB <49827182+bnbbinh1993@users.noreply.github.com>
2024-10-27 21:45:32 +0700

    Create notpixel.js

---
+++ notpixel.js
@@ -0,0 +1,15 @@
+const config_notpixel = {
+  "timeRerun": 1440,
+  "numberThread": 20,
+  "ref_code": 1863187931,
+  "totalPoints": 3000,
+  "maxPoint": 40,
+  "autoTask": true,
+  "autoUpgrade": true,
+  "autoClaim": true,
+  "xTopLeft": 107,
+  "yTopLeft": 282,
+  "xBottomRight": 234,
+  "yBottomRight": 308
+}
+module.exports = { config_notpixel };
