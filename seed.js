fa86037512078e0a4764052360ddb0802b83c3d9
BNB <49827182+bnbbinh1993@users.noreply.github.com>
2024-11-01 02:20:26 +0700

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,4 +1,12 @@
 const config_seed = {
   "d0703548-040d-4527-a97d-647d26e172b0": "Cryptocurrency",
+  "ace94ccd-be20-43e3-a105-60dd80717ecb":"TON",
+  "c39fb00a-2c2e-451b-87ca-23d65e00eacf":"Tokens",
+  "4663208c-551f-4cfe-b95a-63e5deece870":"Airdrop",
+  "7c7bffa3-cd52-4443-8647-4d2201cb7c7a":"Wallet",
+  "39e322d0-df19-49de-92d9-7675170cd65f":"Blockchain",
+  
+  
+  
 }
 module.exports = { config_seed };
