3e65b5ea2ffec359ff35299ac6d94e02bdbab2d7
BNB <49827182+bnbbinh1993@users.noreply.github.com>
2024-11-04 23:18:24 +0700

    Update notpixel.js

--- notpixel.js
+++ notpixel.js
@@ -1,20 +1,35 @@
-const config_notpixel = [
-    {
-    "color": "#FFFFFF",
-    "templateId":1270595015,
-    "xTopLeft":837,
-    "yTopLeft":330,
-    "xBottomRight":868,
-    "yBottomRight":361,
-  },{
-      {
-    "color": "#FFFFFF",
-    "templateId":1270595015,
-    "xTopLeft":837,
-    "yTopLeft":330,
-    "xBottomRight":868,
-    "yBottomRight":361,
-  }
-  
-]
+const config_notpixel = {
+    task: [
+        "channel:notcoin",
+        "channel:notpixel_channel",
+        "invite3frens",
+        "joinSquad",
+        "leagueBonusGold",
+        "leagueBonusSilver",
+        "makePixelAvatar",
+        "paint20pixels",
+        "x:notcoin",
+        "x:notpixel",
+        "jettonTask",
+    ],
+    pixelImg: [
+        {
+            "color": "#FFFFFF",
+            "templateId": 1270595015,
+            "xTopLeft": 837,
+            "yTopLeft": 330,
+            "xBottomRight": 868,
+            "yBottomRight": 361,
+        },
+        {
+            "color": "#FFFFFF",
+            "templateId": 1270595015,
+            "xTopLeft": 837,
+            "yTopLeft": 330,
+            "xBottomRight": 868,
+            "yBottomRight": 361,
+        }
+    ]
+};
+
 module.exports = { config_notpixel };
