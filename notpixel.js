9e822d968adb79ae27f17bb8616158348d8df086
BNB <49827182+bnbbinh1993@users.noreply.github.com>
2024-11-04 21:44:00 +0700

    Update notpixel.js

--- notpixel.js
+++ notpixel.js
@@ -1,9 +1,20 @@
-const config_notpixel = {
-  "color": "#FFFFFF",
-  "templateId":1270595015,
-  "xTopLeft":837,
-  "yTopLeft":330,
-  "xBottomRight":868,
-  "yBottomRight":361,
-}
+const config_notpixel = [
+    {
+    "color": "#FFFFFF",
+    "templateId":1270595015,
+    "xTopLeft":837,
+    "yTopLeft":330,
+    "xBottomRight":868,
+    "yBottomRight":361,
+  },{
+      {
+    "color": "#FFFFFF",
+    "templateId":1270595015,
+    "xTopLeft":837,
+    "yTopLeft":330,
+    "xBottomRight":868,
+    "yBottomRight":361,
+  }
+  
+]
 module.exports = { config_notpixel };
